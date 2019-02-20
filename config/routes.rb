630847ac79df924bf9866edb5d6bf367f829bb7c
WillieShiotsuki <willieshiotsuki@hotmail.com>
2019-02-19 21:37:40 -0300

    inicio

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :produtos
   resources :enderecos
   resources :clientes
+
+  root to: 'clientes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
